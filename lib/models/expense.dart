f688834a557975239060ad8f704cc5b82c0ad1cf
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-30 16:07:13 +0300

    expense model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,5 +1,14 @@
 import 'package:isar/isar.dart';
 
-class Exepense {
+class Expense {
   Id id = Isar.autoIncrement;
+  late final String name;
+  late final double amount;
+  late final DateTime date;
+
+  Expense({
+    required this.name,
+    required this.amount,
+    required this.date,
+  });
 }
