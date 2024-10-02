4ee2e61196bdb673a555e25a07d0fd2f64e467ab
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-02 11:02:14 +0300

    expense data structure

--- lib/data/expense_data.dart
+++ lib/data/expense_data.dart
@@ -80,5 +80,26 @@ class ExpenseData {
   void deleteExpense(ExpenseItem expense){
     overallExpenseList.remove(expense);
   }
+  //get weekday(mon,tues,wed,thurs,fri,sat,sun)from a dateTime object
+  String getWeekday(DateTime date) {
+    switch (date.weekday) {
+      case 1:
+        return 'Mon';
+      case 2:
+        return 'Tue';
+      case 3:
+        return 'Wed';
+      case 4:
+        return 'Thu';
+      case 5:
+        return 'Fri';
+      case 6:
+        return 'Sat';
+      case 7:
+        return 'Sun';
+      default:
+        return '';
+    }
+  } 
   
 }
