921f1549a70f788f988feb6342de022515c7d614
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-02 10:52:01 +0300

    data structure

---
+++ lib/data/expense_data.dart
@@ -0,0 +1,82 @@
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseData{
+  //list of all expenses
+  List<ExpenseItem> overallExpenseList = [
+   /* Expense(
+      id: 1,
+      title: 'Groceries',
+      amount: 100.0,
+      date: DateTime.now(),
+    ),
+    Expense(
+      id: 2,
+      title: 'Fuel',
+      amount: 50.0,
+      date: DateTime.now(),
+    ),
+    Expense(
+      id: 3,
+      title: 'Rent',
+      amount: 500.0,
+      date: DateTime.now(),
+    ),
+    Expense(
+      id: 4,
+      title: 'Internet',
+      amount: 50.0,
+      date: DateTime.now(),
+    ),
+    Expense(
+      id: 5,
+      title: 'Electricity',
+      amount: 100.0,
+      date: DateTime.now(),
+    ),
+    Expense(
+      id: 6,
+      title: 'Water',
+      amount: 50.0,
+      date: DateTime.now(),
+    ),
+    Expense(
+      id: 7,
+      title: 'Phone',
+      amount: 50.0,
+      date: DateTime.now(),
+    ),
+    Expense(
+      id: 8,
+      title: 'Insurance',
+      amount: 100.0,
+      date: DateTime.now(),
+    ),
+    Expense(
+      id: 9,
+      title: 'Health',
+      amount: 50.0,
+      date: DateTime.now(),
+    ),
+    Expense(
+      id: 10,
+      title: 'Entertainment',
+      amount: 100.0,
+      date: DateTime.now(),
+    ),
+    */
+  ];
+ /* 
+  //get expense list
+  List<Expense> get getExpenses{
+    return expenses;
+  }
+  //add new expense
+  void addExpense(Expense expense){
+    expenses.add(expense);
+  }
+  //delete expense
+  void deleteExpense(int id){
+    expenses.removeWhere((element) => element.id == id);
+  }
+  */
+}
