d4c4a0d48007eb474516abc714cab3198c4c8f81
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-07 10:57:14 +0300

    homepage functionalities

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   void addNewExpense() {
     //add new expense
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text('Add New Expense'),
+              content: Text('Add new expense details'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Add new expense
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Add'),
+                ),
+              ],
+            ));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
