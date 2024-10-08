969c3ccae3779587a1da383cbd6c943813e1ca26
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-08 10:59:11 +0300

    my main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:expense_tracker/data/expense_data.dart';
 import 'package:expense_tracker/pages/home_Page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
+  
   runApp(const MyApp());
 }
 
@@ -11,9 +14,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return ChangeNotifierProvider(
+      create: (context) => ExpenseData(),
+      child: MaterialApp(
+        title: 'Expense Tracker',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage(),
+      ),
     );
+    
   }
 }
