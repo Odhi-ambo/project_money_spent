4511bb3665e74d6e5666c6e8847d5edd3aa1821e
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-12 12:09:55 +0300

    expense tile

--- lib/components/expense_tile.dart
+++ lib/components/expense_tile.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ExpenseTile extends StatelessWidget {
-  const ExpenseTile({super.key});
+  final String name;
+  final double amount;
+  final DateTime date;
+  
+  
+  const ExpenseTile(
+      {super.key,required this.name,required this.amount,required this.date});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return ListTile(
+      title: Text(name),
+      subtitle: Text(date.day.toString() +
+          '/' +
+          date.month.toString() +
+          '/' +
+          date.year.toString()),
+      trailing: Text(amount.toString()),
+    );
   }
 }
