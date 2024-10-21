731b231b5f8269d6fe3d7fd95073e8c3b39b2b70
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-21 10:04:45 +0300

    bar data file

--- lib/bargraph/bar_data.dart
+++ lib/bargraph/bar_data.dart
@@ -1,1 +1,22 @@
-class BarData {}
+class BarData {
+  final double sunAmount;
+  final double monAmount;
+  final double tueAmount;
+  final double wedAmount;
+  final double thuAmount;
+  final double friAmount;
+  final double satAmount;
+
+  BarData({
+    required this.sunAmount,
+    required this.monAmount,
+    required this.tueAmount,
+    required this.wedAmount,
+    required this.thuAmount,
+    required this.friAmount,
+    required this.satAmount,
+  });
+
+  double get totalAmount =>
+      sunAmount + monAmount + tueAmount + wedAmount + thuAmount + friAmount + satAmount;
+}
