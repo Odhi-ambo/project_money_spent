e1dffc8c4a8225978c59467d669df741ef9ceb8f
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-21 10:31:48 +0300

    bar graph file

--- lib/bargraph/bar_graph.dart
+++ lib/bargraph/bar_graph.dart
@@ -2,7 +2,18 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class BarGraph extends StatelessWidget {
-  const BarGraph({super.key});
+  final double maxY;
+  final double sunAmount;
+  final double monAmount;
+  final double tueAmount;
+  final double wedAmount;
+  const BarGraph(
+      {super.key,
+      required this.maxY,
+      required this.sunAmount,
+      required this.monAmount,
+      required this.tueAmount,
+      required this.wedAmount});
 
   @override
   Widget build(BuildContext context) {
