4d9f9a0024e2911241edeef7dc948ba57732180e
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-21 11:00:18 +0300

    expense summary

--- lib/components/expense_summary.dart
+++ lib/components/expense_summary.dart
@@ -1,10 +1,14 @@
+import 'package:expense_tracker/data/expense_data.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ExpenseSummary extends StatelessWidget {
-  const ExpenseSummary({super.key});
+  final DateTime startOfWeek;
+  const ExpenseSummary({super.key, required this.startOfWeek});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Consumer<ExpenseData>(
+        builder: (context, value, child) => SizedBox());
   }
 }
