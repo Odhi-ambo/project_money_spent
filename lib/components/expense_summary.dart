8a1bb6f259ab0db90ef73478100f54f200006a55
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-22 10:31:17 +0300

    expense summary

--- lib/components/expense_summary.dart
+++ lib/components/expense_summary.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/bargraph/bar_graph.dart';
 import 'package:expense_tracker/data/expense_data.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,6 +10,18 @@ class ExpenseSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<ExpenseData>(
-        builder: (context, value, child) => SizedBox(height: 200));
+      builder: (context, value, child) => SizedBox(
+        height: 200,
+        child: BarGraph(
+            maxY: 100,
+            sunAmount: 20,
+            monAmount: 40,
+            tueAmount: 60,
+            wedAmount: 80,
+            thuAmount: 100,
+            friAmount: 120,
+            satAmount: 140),
+      ),
+    );
   }
 }
