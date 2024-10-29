6c6a472be96d5abd4a0cc01b7bc1b73cc07e982e
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-29 15:51:39 +0300

    hive db

--- lib/data/hive_database.dart
+++ lib/data/hive_database.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense_item.dart';
 import 'package:hive/hive.dart';
 
 class HiveDatabase {
@@ -5,6 +6,7 @@ class HiveDatabase {
   final _myBox = Hive.box('expense_database');
 
   //write data
+  void saveData(List<ExpenseItem> allExpense){}
 
   //read data
 }
