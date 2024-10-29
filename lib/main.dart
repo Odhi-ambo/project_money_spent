278e8c282d7c829b15e45c154f15818756cf2785
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-29 15:37:48 +0300

    main file commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:expense_tracker/data/expense_data.dart';
 import 'package:expense_tracker/pages/home_Page.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  //initiale hive
+  await Hive.initFlutter();
+
+  //open a hive box
+  await Hive.openBox("expense_database");
+
   runApp(const MyApp());
 }
 
