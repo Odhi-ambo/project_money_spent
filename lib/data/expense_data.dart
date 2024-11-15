eba2d65dd0748bfe780a0a8c8a4a28879d95d7ce
Odhi-ambo <michaelderrick605@gmail.com>
2024-11-15 12:07:24 +0300

    expense data

--- lib/data/expense_data.dart
+++ lib/data/expense_data.dart
@@ -5,80 +5,18 @@ import 'package:expense_tracker/models/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseData extends ChangeNotifier {
-  //list of all expenses
-  List<ExpenseItem> overallExpenseList = [
-    /* Expense(
-      id: 1,
-      title: 'Groceries',
-      amount: 100.0,
-      date: DateTime.now(),
-    ),
-    Expense(
-      id: 2,
-      title: 'Fuel',
-      amount: 50.0,
-      date: DateTime.now(),
-    ),
-    Expense(
-      id: 3,
-      title: 'Rent',
-      amount: 500.0,
-      date: DateTime.now(),
-    ),
-    Expense(
-      id: 4,
-      title: 'Internet',
-      amount: 50.0,
-      date: DateTime.now(),
-    ),
-    Expense(
-      id: 5,
-      title: 'Electricity',
-      amount: 100.0,
-      date: DateTime.now(),
-    ),
-    Expense(
-      id: 6,
-      title: 'Water',
-      amount: 50.0,
-      date: DateTime.now(),
-    ),
-    Expense(
-      id: 7,
-      title: 'Phone',
-      amount: 50.0,
-      date: DateTime.now(),
-    ),
-    Expense(
-      id: 8,
-      title: 'Insurance',
-      amount: 100.0,
-      date: DateTime.now(),
-    ),
-    Expense(
-      id: 9,
-      title: 'Health',
-      amount: 50.0,
-      date: DateTime.now(),
-    ),
-    Expense(
-      id: 10,
-      title: 'Entertainment',
-      amount: 100.0,
-      date: DateTime.now(),
-    ),
-    */
-  ];
-
   // Get expense list
+  List<ExpenseItem> overallExpenseList = [];
+
   List<ExpenseItem> getAllExpenseList() {
     return overallExpenseList;
   }
+
   //prepare data to be displayed in the chart
   final db = HiveDatabase();
-  void prepareData(){
+  void prepareData() {
     //if their exists data, get it
-    if (db.readData().isEmpty){
+    if (db.readData().isEmpty) {
       overallExpenseList = db.readData();
     }
 
